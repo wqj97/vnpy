9dbf31734047e3f9f94bff1dd7104c2e22fd83a8
Wanqj <wqj97@126.com>
2018-05-17 16:41:20 +0800

    改变

--- examples/QuantosDataService/dataService.py
+++ examples/QuantosDataService/dataService.py
@@ -10,7 +10,7 @@ from pymongo import MongoClient, ASCENDING
 from vnpy.trader.vtObject import VtBarData
 from vnpy.trader.app.ctaStrategy.ctaBase import MINUTE_DB_NAME
 from vnpy.trader.gateway.tkproGateway.DataApi import DataApi
-
+from multiprocessing.dummy import Pool
 
 # 交易所类型映射
 exchangeMap = {}
@@ -20,14 +20,12 @@ exchangeMap['CZCE'] = 'CZC'
 exchangeMap['DCE'] = 'DCE'
 exchangeMap['SSE'] = 'SH'
 exchangeMap['SZSE'] = 'SZ'
-exchangeMapReverse = {v:k for k,v in exchangeMap.items()}
+exchangeMapReverse = {v: k for k, v in exchangeMap.items()}
 
 # 加载配置
 config = open('config.json')
 setting = json.load(config)
 config.close()
-
-
 
 MONGO_HOST = setting['MONGO_HOST']
 MONGO_PORT = setting['MONGO_PORT']
@@ -37,17 +35,18 @@ TOKEN = setting['TOKEN']
 DATA_SERVER = setting['DATA_SERVER']
 
 # 创建API对象
-mc = MongoClient(MONGO_HOST, MONGO_PORT)        # Mongo连接
-db = mc[MINUTE_DB_NAME]                         # 数据库
+mc = MongoClient(MONGO_HOST, MONGO_PORT)  # Mongo连接
+db = mc[MINUTE_DB_NAME]  # 数据库
+collections = db.collection_names()
 
 
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 def generateVtBar(row):
     """生成K线"""
     bar = VtBarData()
-    
+
     symbol, exchange = row['symbol'].split('.')
-    
+
     bar.symbol = symbol
     bar.exchange = exchangeMapReverse[exchange]
     bar.vtSymbol = '.'.join([bar.symbol, bar.exchange])
@@ -56,91 +55,97 @@ def generateVtBar(row):
     bar.low = row['low']
     bar.close = row['close']
     bar.volume = row['volume']
-    
+
     bar.date = str(row['date'])
     bar.time = str(row['time']).rjust(6, '0')
-   
-    #将bar的时间改成提前一分钟
-    hour=bar.time[0:2]
-    minute=bar.time[2:4]
-    sec=bar.time[4:6]
-    if minute=="00":
-        minute="59"
-        
+
+    # 将bar的时间改成提前一分钟
+    hour = bar.time[0:2]
+    minute = bar.time[2:4]
+    sec = bar.time[4:6]
+    if minute == "00":
+        minute = "59"
+
         h = int(hour)
         if h == 0:
             h = 24
-        
-        hour=str(h-1).rjust(2,'0')
+
+        hour = str(h - 1).rjust(2, '0')
     else:
-        minute=str(int(minute)-1).rjust(2,'0')
-    bar.time=hour+minute+sec
-   
+        minute = str(int(minute) - 1).rjust(2, '0')
+    bar.time = hour + minute + sec
+
     bar.datetime = datetime.strptime(' '.join([bar.date, bar.time]), '%Y%m%d %H%M%S')
-    
+
     return bar
 
-#----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
 def downMinuteBarBySymbol(api, vtSymbol, startDate, endDate=''):
     """下载某一合约的分钟线数据"""
     start = time()
-    
     code, exchange = vtSymbol.split('.')
+    print('正在下载合约: {}'.format(code))
 
-    # 对于期货合约的vtSymbol没有交易所后缀
     if exchange in ['SSE', 'SZSE']:
         cl = db[vtSymbol]
     else:
         cl = db[code]
+    cl.ensure_index([('datetime', ASCENDING)], unique=True)  # 添加索引
 
-    cl.ensure_index([('datetime', ASCENDING)], unique=True)         # 添加索引
-    
     dt = datetime.strptime(startDate, '%Y%m%d')
-    
+
     if endDate:
         end = datetime.strptime(endDate, '%Y%m%d')
     else:
         end = datetime.now()
     delta = timedelta(1)
-    
-    symbol = '.'.join([code, exchangeMap[exchange]]) 
-    
+    symbol = '.'.join([code, exchangeMap[exchange]])
+    latest_day = cl.find().sort('datetime', -1).limit(1)
+
+    if latest_day.count() != 0:
+        latest_day = latest_day[0]['datetime']
+        latest_day = datetime(latest_day.year, latest_day.month, latest_day.day)
+        print("上次合约: {} 更新到了: {}, 正在同步新的内容".format(code, latest_day))
+        dt = latest_day
+
     while dt <= end:
         d = int(dt.strftime('%Y%m%d'))
         df, msg = api.bar(symbol, freq='1M', trade_date=d)
         dt += delta
-        
+
         if df is None:
             continue
-        
+
         for ix, row in df.iterrows():
             bar = generateVtBar(row)
             d = bar.__dict__
             flt = {'datetime': bar.datetime}
-            cl.replace_one(flt, d, True)    
-        
-
+            cl.replace_one(flt, d, True)
     e = time()
     cost = (e - start) * 1000
 
-    print u'合约%s数据下载完成%s - %s，耗时%s毫秒' %(vtSymbol, startDate, end.strftime('%Y%m%d'), cost)
+    print u'合约%s数据下载完成%s - %s，耗时%s毫秒' % (vtSymbol, startDate, end.strftime('%Y%m%d'), cost)
 
-    
-#----------------------------------------------------------------------
-def downloadAllMinuteBar(api, days=10):
+
+# ----------------------------------------------------------------------
+def downloadAllMinuteBar(api):
     """下载所有配置中的合约的分钟线数据"""
     print '-' * 50
     print u'开始下载合约分钟线数据'
     print '-' * 50
-    
-    startDt = datetime.today() - days * timedelta(1)
-    startDate = startDt.strftime('%Y%m%d')
-    
+
+    poll = Pool(10)
     # 添加下载任务
     for symbol in SYMBOLS:
-        downMinuteBarBySymbol(api, str(symbol), startDate)
-    
+        startDt = symbol.split('.')[0][-4:]
+        startDt = datetime(2000 + int(startDt[0:2]) - 1, int(startDt[2:5]), 1)
+        startDate = startDt.strftime('%Y%m%d')
+        poll.apply_async(downMinuteBarBySymbol, (api, str(symbol), startDate))
+        print('')
+        sleep(0.2)
+    poll.close()
+    poll.join()
     print '-' * 50
     print u'合约分钟线数据下载完成'
     print '-' * 50
-    
--- vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/DependInfo.cmake
+++ vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/DependInfo.cmake
@@ -4,29 +4,29 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/ContractCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/ContractCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/DefaultEWrapper.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/DefaultEWrapper.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EClient.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EClient.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EClientSocket.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EClientSocket.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EDecoder.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EDecoder.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EMessage.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EMessage.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EMutex.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EMutex.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EReader.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EReader.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EReaderOSSignal.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EReaderOSSignal.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/EReaderWMSignal.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EReaderWMSignal.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/ESocket.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/ESocket.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/MarginCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/MarginCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/OperatorCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/OperatorCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/OrderCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/OrderCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/PercentChangeCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/PercentChangeCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/PriceCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/PriceCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/SoftDollarTier.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/SoftDollarTier.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/StdAfx.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/StdAfx.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/TimeCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/TimeCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/VolumeCondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/VolumeCondition.cpp.o"
-  "/home/vnpy/桌面/new/vn.ib/ibapi/linux/client/executioncondition.cpp" "/home/vnpy/桌面/new/vn.ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/executioncondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/ContractCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/ContractCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/DefaultEWrapper.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/DefaultEWrapper.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EClient.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EClient.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EClientSocket.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EClientSocket.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EDecoder.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EDecoder.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EMessage.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EMessage.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EMutex.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EMutex.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EReader.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EReader.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EReaderOSSignal.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EReaderOSSignal.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/EReaderWMSignal.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/EReaderWMSignal.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/ESocket.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/ESocket.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/MarginCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/MarginCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/OperatorCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/OperatorCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/OrderCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/OrderCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/PercentChangeCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/PercentChangeCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/PriceCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/PriceCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/SoftDollarTier.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/SoftDollarTier.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/StdAfx.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/StdAfx.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/TimeCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/TimeCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/VolumeCondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/VolumeCondition.cpp.o"
+  "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/client/executioncondition.cpp" "/Users/wanqianjun/Desktop/vnpy/vnpy/api/ib/ibapi/linux/build/CMakeFiles/twsapi.dir/client/executioncondition.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
