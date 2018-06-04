0de7bb211c7cc426cb3b3f90a1a8b5119a34fc4e
Wanqj <wqj97@126.com>
2018-06-05 00:46:11 +0800

    2018-06-04 优化实时数据

--- examples/QuantosDataService/live.py
+++ examples/QuantosDataService/live.py
@@ -1,18 +1,19 @@
 # coding=utf-8
 import json
 import multiprocessing.dummy as mp
+import random
 import time
 import traceback
-import random
+
 from flask import Flask, request, abort
 from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
-import Queue
+
 import dataService
 from vnpy.trader.gateway.tkproGateway.DataApi import DataApi
 
-pool = mp.Pool(8)
+# pool = mp.Pool(2)
 
 config = open('config.json')
 setting = json.load(config)
@@ -31,30 +32,42 @@ if not info:
 app = Flask(__name__)
 
 client_list = []
+code_data = {}
 
 
 @app.route('/')
 def echo():
     if request.environ.get('wsgi.websocket'):
         ws = request.environ['wsgi.websocket']
-        print("新的连接加入")
-        client_list.append(ws)
+        code = json.loads(request.args['code'])
+        for symbol in code:
+            if symbol not in code_data:
+                code_data[symbol] = 1
+                api.subscribe(symbol, on_quote)
+            else:
+                code_data[symbol] += 1
+        client_list.append((ws, code))
+        print("新的连接加入, 当前订阅列表: {}, 客户端数: {}".format(','.join(code_data), len(client_list)))
         if ws is None:
             abort(404)
         else:
             while True:
                 if not ws.closed:
                     message = ws.receive()
-                    ws.send(message)
-
-
-def send_message(clent, message):
-    try:
-        clent.send(message)
-    except WebSocketError:
-        client_list.remove(clent)
-    except Exception, e:
-        traceback.print_exc(e)
+                    try:
+                        ws.send(message)
+                    except WebSocketError:
+                        for key, (client, code) in enumerate(client_list):
+                            if client is ws:
+                                del client_list[key]
+                                # for symbol in code:
+                                #     code_data[symbol] -= 1
+                                #     if code_data[symbol] <= 0:
+                                #         print("已经没有请求{}的客户端, 取消订阅".format(symbol))
+                                #         api.unsubscribe(symbol)
+                        ws.close()
+                        print('已删除失效的连接, 当前客户端数: {}'.format(len(client_list)))
+                        return 'close'
 
 
 def on_quote(k, v):
@@ -63,24 +76,25 @@ def on_quote(k, v):
     d['datetime'] = bar.datetime.strftime("%Y-%m-%d-%H")
     message = json.dumps(d)
 
-    for clent in client_list:
-        send_message(clent, message)
+    for clent, code in client_list:
+        if d['symbol'] in code:
+            clent.send(message)
 
 
-df, msg = api.bar("rb1901.SHF, hc1901.SHF", freq='1M', trade_date=20180528)
+# df, msg = api.bar("rb1901.SHF, hc1901.SHF", freq='1M', trade_date=20180528)
 
 
-def send_mook_data():
-    count_df = len(df)
-    while True:
-        on_quote(None, df.iloc[random.randrange(0, count_df)])
-        time.sleep(random.randint(1, 10) / 10)
+# def send_mook_data():
+#     count_df = len(df)
+#     while True:
+#         on_quote(None, df.iloc[random.randrange(0, count_df)])
+#         time.sleep(random.randint(1, 10) / 10)
 
 
 if __name__ == '__main__':
     http_server = WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
-    pool.apply_async(send_mook_data)
-    # code, msg = api.subscribe('hc1810.SHF, rb1810.SHF', func=on_quote)
+    # pool.apply_async(send_mook_data)
+    code, msg = api.subscribe(','.join(code_data.keys()), func=on_quote)
     http_server.serve_forever()
 
     # code, msg = api.subscribe('hc1810.SHF, rb1810.SHF', func=on_quote)
